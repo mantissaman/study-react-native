cfc67eb1bf196ca44652f285ddc417bf57444429
Atul Sharma <asharma@sw-at.com>
2019-03-23 21:06:03 +0000

    Custom UI elements

---
+++ Better/src/components/UI/DefaultInput/DefaultInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+
+const defaultInput =(props) =>(
+    <TextInput 
+    style={styles.input} 
+    underlineColorAndroid="transparent"
+    {...props}></TextInput>
+);
+const styles = StyleSheet.create({
+    input:{
+        width:"100%",
+        borderWidth: 1,
+        borderColor:"#eee",
+        padding:5,
+        margin:8
+    }
+});
+
+export default defaultInput;
--- Better/src/screens/Auth/Auth.js
+++ Better/src/screens/Auth/Auth.js
@@ -1,18 +1,36 @@
-import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
-import startMainTabs from '../MainTabs/startMainTabs'
+import React, { Component } from 'react';
+import { View, Text, Button, TextInput, StyleSheet, Platform } from 'react-native';
+import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 
-class AuthScreen extends Component{
-    loginHandler =() =>{
+class AuthScreen extends Component {
+    loginHandler = () => {
         startMainTabs();
     }
-    render(){
-        return(
-                <View>
-                    <Button title="Login" onPress={this.loginHandler}></Button>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Please Log In</Text>
+                <Button title="Switch to Login"></Button>
+                <View style={styles.inputContainer}>
+                    <DefaultInput placeholder="Your E-Mail Address"></DefaultInput>
+                    <DefaultInput placeholder="Password"></DefaultInput>
+                    <DefaultInput placeholder="Confirm Password"></DefaultInput>
                 </View>
+                <Button title="Submit" onPress={this.loginHandler}></Button>
+            </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, //take all available space
+        justifyContent: "center",
+        alignItems: "center" //cross flex direction
+    },
+    inputContainer:{
+        width:"80%"
+    }
+});
 
 export default AuthScreen;
